cedc567dd4b58a1e22add3757fa6db2ff8248815
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-10-11 21:57:57 +1100

    newspline

--- src/components/SplineComponent.js
+++ src/components/SplineComponent.js
@@ -5,7 +5,7 @@ import Spline from "@splinetool/react-spline";
 export default function SplineComponent() {
   return (
     <Spline
-      scene="https://prod.spline.design/d2e4evzkHpc5mxBX/scene.splinecode"
+      scene="https://prod.spline.design/HcAIsMw00zxxmAsz/scene.splinecode"
       className="rounded-xl max-h-96 h-screen shadow-2xl"
     />
   );
