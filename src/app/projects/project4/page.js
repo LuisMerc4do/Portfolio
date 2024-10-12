5e95f2f1e9022f5336996f2421f2c8eb36ddd429
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-10-12 11:45:47 +1100

    ChatPdf

--- src/app/projects/project4/page.js
+++ src/app/projects/project4/page.js
@@ -7,14 +7,23 @@ const project4 = () => {
       <div className="lg:w-6/12 flex flex-col">
         <CardDescription></CardDescription>
         <h2 className="text-6xl font-bold text-center">
-          Law Firm App - a legal firm management System
+          PDF Reader AI - a saas to chat with any PDF
         </h2>
         <div className="text-xl parsed-container flex flex-col mt-10 gap-10 text-justify">
-          A robust backend solution for law firm management, built with ASP.NET.
-          It features secure authentication with JWT, efficient caching with
-          IMemoryCache, comprehensive logging with Serilog, and reliable data
-          storage with PostgreSQL. Deployed with azure devops and using UNIT
-          TESTING
+          End-to-end SaaS application built with modern technologies and best
+          practices. It is designed to be scalable, user-friendly, and
+          efficient, providing a solid foundation for any SaaS business. Create
+          with nextjs and typescript. Complete SaaS Built From Scratch: A robust
+          and scalable SaaS application developed from the ground up. Streaming
+          API Responses in Real-Time: Real-time streaming of API responses for a
+          dynamic user experience.Optimistic UI Updates for a Great UX:
+          Implementing optimistic UI updates to provide a smooth and responsive
+          user experience.
+        </div>
+        <div className="w-full mx-auto">
+          <img src="/images/chatpdf/3.webp" alt="chatpdf" />
+          <img src="/images/chatpdf/2.webp" alt="chatpdf" />
+          <img src="/images/chatpdf/1.webp" alt="chatpdf" />
         </div>
       </div>
     </div>
